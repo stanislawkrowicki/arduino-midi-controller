edd679ee85d2c589e312897aec12a6d7cff8b09e
Stanislaw Krowicki <krowicki00@gmail.com>
2024-05-07 12:58:08 +0200

    Create serial_to_midi tool, creating a virtual midi device

---
+++ serial_to_midi/serial_to_midi/serial_to_midi.c
@@ -0,0 +1,38 @@
+﻿#include <windows.h>
+#include <stdio.h>
+
+#pragma comment(lib, "winmm.lib")
+
+int main() {
+    HMIDISTRM hMidiDevice;
+    MMRESULT result;
+    UINT deviceID = 0;
+
+    printf("Which device to open? (empty for 0): ");
+    char input[10];
+
+    if (fgets(input, sizeof(input), stdin) != NULL) {
+        if (input[0] != '\n') {
+            deviceID = atoi(input);
+        }
+    }
+
+    UINT IDCopy = deviceID;
+
+    result = midiStreamOpen(&hMidiDevice,
+        &deviceID,
+        1,           
+        (DWORD_PTR)NULL,
+        (DWORD_PTR)NULL,
+        CALLBACK_NULL);
+
+    if (result != MMSYSERR_NOERROR) {
+        printf("ERROR: Failed to open MIDI device with ID %u! Error code: %u\n", IDCopy, result);
+        return -1;
+    }
+
+    printf("Device %d opened succesfully.\n", deviceID);
+    midiStreamClose(hMidiDevice);
+
+    return 0;
+}
