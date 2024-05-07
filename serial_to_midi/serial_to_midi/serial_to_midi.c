e521d1914ad14a49b3db260d951a9e597c9923af
Stanislaw Krowicki <krowicki00@gmail.com>
2024-05-07 19:33:52 +0200

    Opening midi out device instead of stream

--- serial_to_midi/serial_to_midi/serial_to_midi.c
+++ serial_to_midi/serial_to_midi/serial_to_midi.c
@@ -4,8 +4,25 @@
 #pragma comment(lib, "winmm.lib")
 
 int main() {
-    HMIDISTRM hMidiDevice;
+    UINT numMidiOutDevices = midiOutGetNumDevs();
+    MIDIOUTCAPS midiOutCaps;
     MMRESULT result;
+
+    printf("Number of MIDI Input Devices: %u\n", numMidiOutDevices);
+
+    for (UINT i = 0; i < numMidiOutDevices; ++i) {
+        result = midiOutGetDevCapsA(i, &midiOutCaps, sizeof(MIDIOUTCAPS));
+        if (result == MMSYSERR_NOERROR) {
+            printf("Device ID: %u\n", i);
+            printf("Name: %s\n", midiOutCaps.szPname);
+        }
+        else {
+            printf("Failed to get device capabilities for device ID %u\n", i);
+        }
+    }
+
+    HMIDIOUT hMidiDevice;
+    //MMRESULT result;
     UINT deviceID = 0;
 
     printf("Which device to open? (empty for 0): ");
@@ -19,20 +36,24 @@ int main() {
 
     UINT IDCopy = deviceID;
 
-    result = midiStreamOpen(&hMidiDevice,
+    result = midiOutOpen(&hMidiDevice,
         &deviceID,
         1,           
         (DWORD_PTR)NULL,
         (DWORD_PTR)NULL,
         CALLBACK_NULL);
 
+
     if (result != MMSYSERR_NOERROR) {
-        printf("ERROR: Failed to open MIDI device with ID %u! Error code: %u\n", IDCopy, result);
+        char errorText[1024];
+        midiOutGetErrorTextA(result, errorText, sizeof(errorText));
+        printf("ERROR: Failed to open MIDI device with ID %u! Error code: %u, Message: %s\n", IDCopy, result, errorText);
+        Sleep(5000);
         return -1;
     }
 
     printf("Device %d opened succesfully.\n", deviceID);
-    midiStreamClose(hMidiDevice);
+    Sleep(5000);
 
     return 0;
 }
